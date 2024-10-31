515aaf1373c490d99936b5e9f4f92dc47a891008
Dean Betty <deanlloyd64@gmail.com>
2024-10-30 23:16:28 -0400

    update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from '@jest/types';
+import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+	clearMocks: true,
+	collectCoverage: true,
+
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest",
+		"^.+\\.(js|jsx)$": "babel-jest",
+	},
+	transformIgnorePatterns: [
+		"/node_modules/(?!(d3-delaunay|d3-path|delaunator|robust-predicates)/)",
+	],
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
+	setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+	testMatch: [
+		"**/__tests__/**/*.[jt]s?(x)",
+		"**/?(*.)+(spec|test).[jt]s?(x)",
+	],
 };
 
 export default config;
